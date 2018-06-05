b298529f779bdfec2c0fc0270ecec0bd92edffa5
Dev Lopment <dev2.twinbit@gmail.com>
2018-06-05 10:05:37 +0600

    Initial Commit

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by Saad on 5/6/18.
+//  Copyright © 2018 Saad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Saad on 5/6/18.
+//  Copyright © 2018 Saad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
